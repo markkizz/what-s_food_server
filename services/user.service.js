19198cd96f00b07284878d119cf541ab10c3d8eb
markkizz <mark.pattanapara@gmail.com>
2019-12-28 11:40:19 +0700

    [Add eslint for impove syntax]

--- services/user.service.js
+++ services/user.service.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const config = require('../config/passport/passport');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 
 module.exports = (app, db) => {
-
-  app.post('/userRegister', (req, res, next) => {
+  app.post('/registerUser', (req, res, next) => {
     passport.authenticate('register', (err, user, info) => {
+      const { first_name, last_name, email } = req.body;
       if (err) {
         console.error(err);
       }
@@ -15,37 +15,37 @@ module.exports = (app, db) => {
         res.status(403).send(info.message);
       } else {
         const data = {
-          first_name: req.body.first_name,
-          last_name: req.body.last_name,
-          email: req.body.email,
+          first_name,
+          last_name,
+          email,
           username: user.username,
-          role: "user"
+          role: 'user'
         };
         console.log(data);
-        db.user.findOne({
-          where: {
-            username: data.username,
-          }
-        }).then(user => {
-          console.log(user);
-          user
-            .update({
-              first_name: data.first_name,
-              last_name: data.last_name,
-              email: data.email,
-              role: data.role
-            })
-            .then(() => {
-              console.log('user created in db');
-              res.status(200).send({ message: 'user created' });
-            });
-        })
-          .catch(err => {
-            console.log(err)
+        db.user
+          .findOne({
+            where: {
+              username: data.username
+            }
           })
-
+          .then(foundUser => {
+            console.log(foundUser);
+            foundUser
+              .update({
+                first_name: data.first_name,
+                last_name: data.last_name,
+                email: data.email,
+                role: data.role
+              })
+              .then(() => {
+                console.log('user created in db');
+                res.status(200).send({ message: 'user created' });
+              });
+          })
+          .catch(error => {
+            console.log('error message: ', error);
+          });
       }
-    })(req, res, next)
-  }
-
-}
+    })(req, res, next);
+  });
+};
